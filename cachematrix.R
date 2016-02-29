22fd54b3c86d47da2596c21c81420e35326ab730
Alias Eldhose <aliaseldhose@ceegees.in>
2016-02-29 11:40:04 +0530

    Adding the solution for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## File contain two methods for finding the inverse of a given matrix and cache 
+## the result so that there is no need to find the inverse again for the same matrix
 
-## Write a short comment describing this function
-
+##
+# Function to generate a list of function to handle the environment level variables for caching
+##
 makeCacheMatrix <- function(x = matrix()) {
 
+	inv <- NULL
+	## Set the matrix in local scope
+	mat <<- x
+
+	##
+	# Get the original matrix
+	##
+	get <- function() {
+		mat
+	}
+
+	##
+	# Set the inverse matrix in local scope
+	setInverse <- function(mInv)  {
+		inv <<- mInv
+	}
+
+	##
+	# Get the inverse from the local scope
+	##
+	getInverse <- function() {
+		inv
+	}
+
+	# Return the list list defined functions
+	list(get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+##
+# Function to generate the matrix inverse and store it in the cache, so that 
+# on a second request to get the inverse (for the same matrix) it is served from cache
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# Try to get the inverse
+	inverse <- x$getInverse()
+
+	# Return the inverse if already cached
+	if(!is.null(inverse)) {
+		message("getting inverse from the cache")
+		# Return the inverse
+		return(inverse)
+	}
+
+	## Create the inverse and cache it if 'inverse' is null
+
+	# Get the original Matrix
+	mat <- x$get()
+	# Find the inverse of original matrix
+	inverse <- solve(mat)
+	# Store the inverse in cache
+	x$setInverse(inverse)
+
+	# return the inverse
+	inverse
 }
